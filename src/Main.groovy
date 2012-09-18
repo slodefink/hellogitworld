8b4c464fbb14c7511c7da5a60e1f055566f265f8
stephen lodefink <stephen.lodefink@disney.com>
2012-09-18 08:53:12 -0700

    commiting main .groovy changes

--- src/Main.groovy
+++ src/Main.groovy
@@ -3,7 +3,7 @@ import static Division.divide
 import static Subtract.subtract
 import static Sum.sum
 
-def name = "Matthew"
+def name = "steve"
 int programmingPoints = 10
 
 println "Hello ${name}"
@@ -11,4 +11,4 @@ println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${square(programmingPoints)}"
 println "${programmingPoints} divided by 2 bonus points is ${divide(programmingPoints, 2)}"
 println "${programmingPoints} minus 7 bonus points is ${subtract(programmingPoints, 7)}"
-println "${programmingPoints} plus 3 bonus points is ${sum(programmingPoints, 3)}"
+println "${programmingPoints} plus 3 bonus points is ${sum(programmingPoints, 3)}"/Users/stephenl/Git/hellogitworld/main.groovy
